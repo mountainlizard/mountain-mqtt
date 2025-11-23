1a36a047fae7ce3eea1e5beed7d989299c0754b8
trepidacious <trepidacious@gmail.com>
2025-11-23 11:56:07 +0000

    Add more client methods

--- embassy-poll-example-rp2040w/src/packet_bin_proto.rs
+++ embassy-poll-example-rp2040w/src/packet_bin_proto.rs
@@ -8,19 +8,25 @@ use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::Channel;
 use embassy_sync::channel::Receiver;
 use embassy_sync::channel::Sender;
-use embassy_time::{Delay, Timer};
-use embedded_hal_async::delay::DelayNs;
+use embassy_time::Timer;
 use embedded_io_async::Write;
+use heapless::Vec;
+use mountain_mqtt::client::ClientError;
+use mountain_mqtt::client::ConnectionSettings;
+use mountain_mqtt::client_state::ClientState;
+use mountain_mqtt::client_state::ClientStateNoQueue;
 use mountain_mqtt::codec::mqtt_writer::{MqttBufWriter, MqttWriter};
 use mountain_mqtt::codec::write;
-use mountain_mqtt::error::PacketWriteError;
+use mountain_mqtt::data::property::ConnectProperty;
 use mountain_mqtt::packets::connect::Connect;
+use mountain_mqtt::packets::connect::Will;
 use mountain_mqtt::packets::packet::Packet;
 use mountain_mqtt_embassy::mqtt_manager::Settings;
 
 use {defmt_rtt as _, panic_probe as _};
 
 pub struct Client<'a, const N: usize> {
+    client_state: ClientStateNoQueue,
     sender: Sender<'a, NoopRawMutex, PacketBin<N>, 1>,
     receiver: Receiver<'a, NoopRawMutex, PacketBin<N>, 1>,
 }
@@ -34,7 +40,7 @@ impl<'a, const N: usize> Client<'a, N> {
         self.receiver.receive().await
     }
 
-    pub async fn send<P>(&mut self, packet: P) -> Result<(), PacketWriteError>
+    pub async fn send<P>(&mut self, packet: P) -> Result<(), ClientError>
     where
         P: Packet + write::Write,
     {
@@ -49,50 +55,96 @@ impl<'a, const N: usize> Client<'a, N> {
         self.send_bin(packet).await;
         Ok(())
     }
+
+    pub async fn connect_with_will<const W: usize>(
+        &mut self,
+        settings: &ConnectionSettings<'_>,
+        will: Option<Will<'_, W>>,
+    ) -> Result<(), ClientError> {
+        let mut properties = Vec::new();
+        // By setting maximum topic alias to 0, we prevent the server
+        // trying to use aliases, which we don't support. They are optional
+        // and only provide for reduced packet size, but would require storing
+        // topic names from the server for the length of the connection,
+        // which might be awkward without alloc.
+        properties
+            .push(ConnectProperty::TopicAliasMaximum(0.into()))
+            .unwrap();
+        let packet: Connect<'_, 1, W> = Connect::new(
+            settings.keep_alive(),
+            *settings.username(),
+            *settings.password(),
+            settings.client_id(),
+            true,
+            will,
+            properties,
+        );
+        self.client_state.connect(&packet)?;
+        self.send(packet).await
+    }
+
+    pub async fn connect(&mut self, settings: &ConnectionSettings<'_>) -> Result<(), ClientError> {
+        self.connect_with_will::<0>(settings, None).await
+    }
+}
+
+pub async fn demo_poll_result(client: &mut Client<'_, 1024>) -> Result<(), ClientError> {
+    client
+        .connect(&ConnectionSettings::unauthenticated("packet_bin_proto"))
+        .await?;
+
+    Ok(())
 }
 
 pub async fn demo_poll(client: &mut Client<'_, 1024>) {
-    let mut index: u64 = 0;
-    loop {
-        info!("demo_poll: About to send packet {}", &index,);
-
-        let packet = Connect::unauthenticated_no_topic_aliases("packet_bin_proto");
-
-        if let Err(e) = client.send(packet).await {
-            info!("demo_poll: Failed to send connect packet {:?}", e);
-        } else {
-            info!("demo_poll: Sent connect packet");
-        }
-
-        info!("demo_poll: About to receive data (or cancel)");
-
-        let received = client.receive_bin().await;
-        match received.as_packet_generic::<16, 16, 16>() {
-            Ok(packet) => {
-                let packet_type = packet.packet_type();
-                info!(
-                    "demo_poll: Received packet type {:?} from channel",
-                    packet_type
-                );
-            }
-            Err(e) => {
-                info!(
-                    "demo_poll: Failed to decode packet from data ({}), ending...",
-                    e
-                );
-                return;
-            }
-        }
-
-        Delay.delay_ms(1000).await;
-
-        index += 1;
-
-        if index >= 10 {
-            info!("demo_poll: Enough data sent, ending...");
-            return;
-        }
+    if let Err(e) = demo_poll_result(client).await {
+        info!("demo_poll: Error {}", e);
     }
+
+    // let connect = Connect::unauthenticated_no_topic_aliases("packet_bin_proto");
+    // client.send(connect).await;
+
+    // let mut index: u64 = 0;
+    // loop {
+    //     info!("demo_poll: About to send packet {}", &index,);
+
+    //     let packet = Connect::unauthenticated_no_topic_aliases("packet_bin_proto");
+
+    //     if let Err(e) = client.send(packet).await {
+    //         info!("demo_poll: Failed to send connect packet {:?}", e);
+    //     } else {
+    //         info!("demo_poll: Sent connect packet");
+    //     }
+
+    //     info!("demo_poll: About to receive data (or cancel)");
+
+    //     let received = client.receive_bin().await;
+    //     match received.as_packet_generic::<16, 16, 16>() {
+    //         Ok(packet) => {
+    //             let packet_type = packet.packet_type();
+    //             info!(
+    //                 "demo_poll: Received packet type {:?} from channel",
+    //                 packet_type
+    //             );
+    //         }
+    //         Err(e) => {
+    //             info!(
+    //                 "demo_poll: Failed to decode packet from data ({}), ending...",
+    //                 e
+    //             );
+    //             return;
+    //         }
+    //     }
+
+    //     Delay.delay_ms(1000).await;
+
+    //     index += 1;
+
+    //     if index >= 10 {
+    //         info!("demo_poll: Enough data sent, ending...");
+    //         return;
+    //     }
+    // }
 }
 
 pub async fn run_with_demo_poll(settings: Settings, stack: Stack<'static>) {
@@ -134,23 +186,11 @@ pub async fn run<const N: usize>(
         let (mut rx, mut tx) = socket.split();
 
         let rx_fut = async {
-            // let mut buf = [0; N];
-
             loop {
                 match packet_io::receive_packet_bin(&mut rx).await {
                     Ok(packet_bin) => rx_channel_sender.send(packet_bin).await,
                     Err(e) => return e,
                 }
-
-                // if let Err(e) = rx.read_exact(&mut buf).await {
-                //     return e;
-                // }
-                // rx_channel_sender
-                //     .send(
-                //         PacketBin::new(&buf)
-                //             .expect("PacketBin size mismatch in rx_fut, should not occur"),
-                //     )
-                //     .await
             }
         };
 
@@ -164,6 +204,7 @@ pub async fn run<const N: usize>(
         };
 
         let mut client = Client {
+            client_state: ClientStateNoQueue::default(),
             sender: tx_channel.sender(),
             receiver: rx_channel.receiver(),
         };
