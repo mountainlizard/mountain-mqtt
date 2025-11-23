6450ab5867eb3b6dbdc9ee643edecaaed68814da
trepidacious <trepidacious@gmail.com>
2025-11-23 09:10:43 +0000

    Comment out older protos for now

--- embassy-poll-example-rp2040w/src/byte_data_proto.rs
+++ embassy-poll-example-rp2040w/src/byte_data_proto.rs
@@ -1,135 +1,135 @@
-use defmt::*;
-use embassy_futures::select::{select3, Either3};
-use embassy_net::tcp::TcpSocket;
-use embassy_net::Stack;
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use embassy_sync::channel::Channel;
-use embassy_sync::channel::Receiver;
-use embassy_sync::channel::Sender;
-use embassy_time::{Delay, Timer};
-use embedded_hal_async::delay::DelayNs;
-use embedded_io_async::Read;
-use embedded_io_async::Write;
-use mountain_mqtt_embassy::mqtt_manager::Settings;
-use {defmt_rtt as _, panic_probe as _};
+// use defmt::*;
+// use embassy_futures::select::{select3, Either3};
+// use embassy_net::tcp::TcpSocket;
+// use embassy_net::Stack;
+// use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+// use embassy_sync::channel::Channel;
+// use embassy_sync::channel::Receiver;
+// use embassy_sync::channel::Sender;
+// use embassy_time::{Delay, Timer};
+// use embedded_hal_async::delay::DelayNs;
+// use embedded_io_async::Read;
+// use embedded_io_async::Write;
+// use mountain_mqtt_embassy::mqtt_manager::Settings;
+// use {defmt_rtt as _, panic_probe as _};
 
-pub struct Client<'a> {
-    sender: Sender<'a, NoopRawMutex, [u8; 8], 1>,
-    receiver: Receiver<'a, NoopRawMutex, [u8; 8], 1>,
-}
+// pub struct Client<'a> {
+//     sender: Sender<'a, NoopRawMutex, [u8; 8], 1>,
+//     receiver: Receiver<'a, NoopRawMutex, [u8; 8], 1>,
+// }
 
-impl<'a> Client<'a> {
-    pub async fn send(&mut self, message: [u8; 8]) {
-        self.sender.send(message).await
-    }
+// impl<'a> Client<'a> {
+//     pub async fn send(&mut self, message: [u8; 8]) {
+//         self.sender.send(message).await
+//     }
 
-    pub async fn receive(&mut self) -> [u8; 8] {
-        self.receiver.receive().await
-    }
-}
+//     pub async fn receive(&mut self) -> [u8; 8] {
+//         self.receiver.receive().await
+//     }
+// }
 
-pub async fn demo_poll(client: &mut Client<'_>) {
-    let mut index: u64 = 0;
-    loop {
-        info!(
-            "demo_poll: About to send data ({}){:?} (or cancel)",
-            &index,
-            &index.to_ne_bytes()
-        );
+// pub async fn demo_poll(client: &mut Client<'_>) {
+//     let mut index: u64 = 0;
+//     loop {
+//         info!(
+//             "demo_poll: About to send data ({}){:?} (or cancel)",
+//             &index,
+//             &index.to_ne_bytes()
+//         );
 
-        client.send(index.to_ne_bytes()).await;
-        info!(
-            "demo_poll: Sent data ({}){:?}",
-            &index,
-            &index.to_ne_bytes()
-        );
+//         client.send(index.to_ne_bytes()).await;
+//         info!(
+//             "demo_poll: Sent data ({}){:?}",
+//             &index,
+//             &index.to_ne_bytes()
+//         );
 
-        info!("demo_poll: About to receive data (or cancel)");
+//         info!("demo_poll: About to receive data (or cancel)");
 
-        let received = client.receive().await;
-        info!("demo_poll: Received data {:?} from channel", &received);
+//         let received = client.receive().await;
+//         info!("demo_poll: Received data {:?} from channel", &received);
 
-        Delay.delay_ms(1000).await;
+//         Delay.delay_ms(1000).await;
 
-        index += 1;
+//         index += 1;
 
-        if index >= 10 {
-            info!("demo_poll: Enough data sent, ending...");
-            return;
-        }
-    }
-}
+//         if index >= 10 {
+//             info!("demo_poll: Enough data sent, ending...");
+//             return;
+//         }
+//     }
+// }
 
-pub async fn run_with_demo_poll(settings: Settings, stack: Stack<'static>) {
-    run(settings, stack, demo_poll).await
-}
+// pub async fn run_with_demo_poll(settings: Settings, stack: Stack<'static>) {
+//     run(settings, stack, demo_poll).await
+// }
 
-// TODO: Move to accepting a trait impl rather than AsyncFn, so it's easier to package up say some
-// queues and provide an async method to run with them?
-pub async fn run(settings: Settings, stack: Stack<'static>, f: impl AsyncFn(&mut Client)) {
-    const B: usize = 1024;
+// // TODO: Move to accepting a trait impl rather than AsyncFn, so it's easier to package up say some
+// // queues and provide an async method to run with them?
+// pub async fn run(settings: Settings, stack: Stack<'static>, f: impl AsyncFn(&mut Client)) {
+//     const B: usize = 1024;
 
-    let mut rx_buffer = [0; B];
-    let mut tx_buffer = [0; B];
+//     let mut rx_buffer = [0; B];
+//     let mut tx_buffer = [0; B];
 
-    loop {
-        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+//     loop {
+//         let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
 
-        socket.set_timeout(None);
+//         socket.set_timeout(None);
 
-        let remote_endpoint = (settings.address, settings.port);
-        info!("MQTT socket connecting to {:?}...", remote_endpoint);
-        // TODO: This should just return directly, to let caller decide whether to retry
-        if let Err(e) = socket.connect(remote_endpoint).await {
-            warn!("MQTT socket connect error, will retry: {:?}", e);
-            // Wait a while to try reconnecting
-            Timer::after(settings.reconnection_delay).await;
-            continue;
-        }
-        info!("MQTT socket connected!");
+//         let remote_endpoint = (settings.address, settings.port);
+//         info!("MQTT socket connecting to {:?}...", remote_endpoint);
+//         // TODO: This should just return directly, to let caller decide whether to retry
+//         if let Err(e) = socket.connect(remote_endpoint).await {
+//             warn!("MQTT socket connect error, will retry: {:?}", e);
+//             // Wait a while to try reconnecting
+//             Timer::after(settings.reconnection_delay).await;
+//             continue;
+//         }
+//         info!("MQTT socket connected!");
 
-        let rx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
-        let rx_channel_sender: embassy_sync::channel::Sender<'_, NoopRawMutex, [u8; 8], 1> =
-            rx_channel.sender();
+//         let rx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
+//         let rx_channel_sender: embassy_sync::channel::Sender<'_, NoopRawMutex, [u8; 8], 1> =
+//             rx_channel.sender();
 
-        let tx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
-        let tx_channel_receiver = tx_channel.receiver();
+//         let tx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
+//         let tx_channel_receiver = tx_channel.receiver();
 
-        let (mut rx, mut tx) = socket.split();
+//         let (mut rx, mut tx) = socket.split();
 
-        let rx_fut = async {
-            let mut buf = [0; 8];
+//         let rx_fut = async {
+//             let mut buf = [0; 8];
 
-            loop {
-                if let Err(e) = rx.read_exact(&mut buf).await {
-                    return e;
-                }
-                rx_channel_sender.send(buf).await
-            }
-        };
+//             loop {
+//                 if let Err(e) = rx.read_exact(&mut buf).await {
+//                     return e;
+//                 }
+//                 rx_channel_sender.send(buf).await
+//             }
+//         };
 
-        let tx_fut = async {
-            loop {
-                let write = tx_channel_receiver.receive().await;
-                if let Err(e) = tx.write_all(&write).await {
-                    return e;
-                }
-            }
-        };
+//         let tx_fut = async {
+//             loop {
+//                 let write = tx_channel_receiver.receive().await;
+//                 if let Err(e) = tx.write_all(&write).await {
+//                     return e;
+//                 }
+//             }
+//         };
 
-        let mut client = Client {
-            sender: tx_channel.sender(),
-            receiver: rx_channel.receiver(),
-        };
+//         let mut client = Client {
+//             sender: tx_channel.sender(),
+//             receiver: rx_channel.receiver(),
+//         };
 
-        info!("About to start tcp futures");
+//         info!("About to start tcp futures");
 
-        match select3(rx_fut, tx_fut, f(&mut client)).await {
-            Either3::First(e) => info!("Finished network comms with read error {:?}", e),
-            Either3::Second(e) => info!("Finished network comms with write error {:?}", e),
-            Either3::Third(_) => info!("Finished network comms by polling completing"),
-        }
+//         match select3(rx_fut, tx_fut, f(&mut client)).await {
+//             Either3::First(e) => info!("Finished network comms with read error {:?}", e),
+//             Either3::Second(e) => info!("Finished network comms with write error {:?}", e),
+//             Either3::Third(_) => info!("Finished network comms by polling completing"),
+//         }
 
-        info!("Finished network comms, will reconnect");
-    }
-}
+//         info!("Finished network comms, will reconnect");
+//     }
+// }
--- embassy-poll-example-rp2040w/src/message_proto.rs
+++ embassy-poll-example-rp2040w/src/message_proto.rs
@@ -1,149 +1,149 @@
-use defmt::*;
-use embassy_futures::select::{select3, Either3};
-use embassy_net::tcp::TcpSocket;
-use embassy_net::Stack;
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use embassy_sync::channel::Channel;
-use embassy_sync::channel::Receiver;
-use embassy_sync::channel::Sender;
-use embassy_sync::pipe::Pipe;
-use embassy_sync::pipe::Reader;
-use embassy_sync::pipe::Writer;
-use embassy_time::{Delay, Timer};
-use embedded_hal_async::delay::DelayNs;
-use embedded_io_async::Read;
-use embedded_io_async::Write;
-use mountain_mqtt_embassy::mqtt_manager::Settings;
-use {defmt_rtt as _, panic_probe as _};
+// use defmt::*;
+// use embassy_futures::select::{select3, Either3};
+// use embassy_net::tcp::TcpSocket;
+// use embassy_net::Stack;
+// use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+// use embassy_sync::channel::Channel;
+// use embassy_sync::channel::Receiver;
+// use embassy_sync::channel::Sender;
+// use embassy_sync::pipe::Pipe;
+// use embassy_sync::pipe::Reader;
+// use embassy_sync::pipe::Writer;
+// use embassy_time::{Delay, Timer};
+// use embedded_hal_async::delay::DelayNs;
+// use embedded_io_async::Read;
+// use embedded_io_async::Write;
+// use mountain_mqtt_embassy::mqtt_manager::Settings;
+// use {defmt_rtt as _, panic_probe as _};
 
-const B: usize = 1024;
+// const B: usize = 1024;
 
-pub struct Client<'a> {
-    // sender: Sender<'a, NoopRawMutex, [u8; 8], 1>,
-    sender: Writer<'a, NoopRawMutex, B>,
-    receiver: Receiver<'a, NoopRawMutex, [u8; 8], 1>,
-}
+// pub struct Client<'a> {
+//     // sender: Sender<'a, NoopRawMutex, [u8; 8], 1>,
+//     sender: Writer<'a, NoopRawMutex, B>,
+//     receiver: Receiver<'a, NoopRawMutex, [u8; 8], 1>,
+// }
 
-impl<'a> Client<'a> {
-    pub async fn send(&mut self, message: [u8; 8]) {
-        // self.sender.send(message).await
-        self.sender
-            .write_all(&message)
-            .await
-            .expect("Failed to write to client pipe, this should be Infallible");
-    }
+// impl<'a> Client<'a> {
+//     pub async fn send(&mut self, message: [u8; 8]) {
+//         // self.sender.send(message).await
+//         self.sender
+//             .write_all(&message)
+//             .await
+//             .expect("Failed to write to client pipe, this should be Infallible");
+//     }
 
-    pub async fn receive(&mut self) -> [u8; 8] {
-        self.receiver.receive().await
-    }
-}
+//     pub async fn receive(&mut self) -> [u8; 8] {
+//         self.receiver.receive().await
+//     }
+// }
 
-pub async fn demo_poll(client: &mut Client<'_>) {
-    let mut index: u64 = 0;
-    loop {
-        info!(
-            "demo_poll: About to send data ({}){:?} (or cancel)",
-            &index,
-            &index.to_ne_bytes()
-        );
+// pub async fn demo_poll(client: &mut Client<'_>) {
+//     let mut index: u64 = 0;
+//     loop {
+//         info!(
+//             "demo_poll: About to send data ({}){:?} (or cancel)",
+//             &index,
+//             &index.to_ne_bytes()
+//         );
 
-        client.send(index.to_ne_bytes()).await;
-        info!(
-            "demo_poll: Sent data ({}){:?}",
-            &index,
-            &index.to_ne_bytes()
-        );
+//         client.send(index.to_ne_bytes()).await;
+//         info!(
+//             "demo_poll: Sent data ({}){:?}",
+//             &index,
+//             &index.to_ne_bytes()
+//         );
 
-        info!("demo_poll: About to receive data (or cancel)");
+//         info!("demo_poll: About to receive data (or cancel)");
 
-        let received = client.receive().await;
-        info!("demo_poll: Received data {:?} from channel", &received);
+//         let received = client.receive().await;
+//         info!("demo_poll: Received data {:?} from channel", &received);
 
-        Delay.delay_ms(1000).await;
+//         Delay.delay_ms(1000).await;
 
-        index += 1;
+//         index += 1;
 
-        if index >= 10 {
-            info!("demo_poll: Enough data sent, ending...");
-            return;
-        }
-    }
-}
+//         if index >= 10 {
+//             info!("demo_poll: Enough data sent, ending...");
+//             return;
+//         }
+//     }
+// }
 
-pub async fn run_with_demo_poll(settings: Settings, stack: Stack<'static>) {
-    run(settings, stack, demo_poll).await
-}
+// pub async fn run_with_demo_poll(settings: Settings, stack: Stack<'static>) {
+//     run(settings, stack, demo_poll).await
+// }
 
-// TODO: Move to accepting a trait impl rather than AsyncFn, so it's easier to package up say some
-// queues and provide an async method to run with them?
-pub async fn run(settings: Settings, stack: Stack<'static>, f: impl AsyncFn(&mut Client)) {
-    let mut rx_buffer = [0; B];
-    let mut tx_buffer = [0; B];
+// // TODO: Move to accepting a trait impl rather than AsyncFn, so it's easier to package up say some
+// // queues and provide an async method to run with them?
+// pub async fn run(settings: Settings, stack: Stack<'static>, f: impl AsyncFn(&mut Client)) {
+//     let mut rx_buffer = [0; B];
+//     let mut tx_buffer = [0; B];
 
-    loop {
-        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+//     loop {
+//         let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
 
-        socket.set_timeout(None);
+//         socket.set_timeout(None);
 
-        let remote_endpoint = (settings.address, settings.port);
-        info!("MQTT socket connecting to {:?}...", remote_endpoint);
-        // TODO: This should just return directly, to let caller decide whether to retry
-        if let Err(e) = socket.connect(remote_endpoint).await {
-            warn!("MQTT socket connect error, will retry: {:?}", e);
-            // Wait a while to try reconnecting
-            Timer::after(settings.reconnection_delay).await;
-            continue;
-        }
-        info!("MQTT socket connected!");
+//         let remote_endpoint = (settings.address, settings.port);
+//         info!("MQTT socket connecting to {:?}...", remote_endpoint);
+//         // TODO: This should just return directly, to let caller decide whether to retry
+//         if let Err(e) = socket.connect(remote_endpoint).await {
+//             warn!("MQTT socket connect error, will retry: {:?}", e);
+//             // Wait a while to try reconnecting
+//             Timer::after(settings.reconnection_delay).await;
+//             continue;
+//         }
+//         info!("MQTT socket connected!");
 
-        let (mut rx, mut tx) = socket.split();
+//         let (mut rx, mut tx) = socket.split();
 
-        let rx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
-        let rx_channel_sender: embassy_sync::channel::Sender<'_, NoopRawMutex, [u8; 8], 1> =
-            rx_channel.sender();
+//         let rx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
+//         let rx_channel_sender: embassy_sync::channel::Sender<'_, NoopRawMutex, [u8; 8], 1> =
+//             rx_channel.sender();
 
-        let rx_fut = async {
-            loop {
-                let mut buf = [0; 8];
-                if let Err(e) = rx.read_exact(&mut buf).await {
-                    return e;
-                }
-                rx_channel_sender.send(buf).await;
-                buf[0] = 1;
-            }
-        };
+//         let rx_fut = async {
+//             loop {
+//                 let mut buf = [0; 8];
+//                 if let Err(e) = rx.read_exact(&mut buf).await {
+//                     return e;
+//                 }
+//                 rx_channel_sender.send(buf).await;
+//                 buf[0] = 1;
+//             }
+//         };
 
-        let mut tx_pipe: Pipe<NoopRawMutex, B> = Pipe::new();
-        let (tx_pipe_reader, tx_pipe_writer) = tx_pipe.split();
+//         let mut tx_pipe: Pipe<NoopRawMutex, B> = Pipe::new();
+//         let (tx_pipe_reader, tx_pipe_writer) = tx_pipe.split();
 
-        // let tx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
-        // let tx_channel_receiver = tx_channel.receiver();
+//         // let tx_channel: Channel<NoopRawMutex, [u8; 8], 1> = Channel::new();
+//         // let tx_channel_receiver = tx_channel.receiver();
 
-        let tx_fut = async {
-            let mut buf = [0; 64];
-            loop {
-                // let write = tx_channel_receiver.receive().await;
-                let n = tx_pipe_reader.read(&mut buf).await;
-                if let Err(e) = tx.write_all(&buf[0..n]).await {
-                    return e;
-                }
-            }
-        };
+//         let tx_fut = async {
+//             let mut buf = [0; 64];
+//             loop {
+//                 // let write = tx_channel_receiver.receive().await;
+//                 let n = tx_pipe_reader.read(&mut buf).await;
+//                 if let Err(e) = tx.write_all(&buf[0..n]).await {
+//                     return e;
+//                 }
+//             }
+//         };
 
-        let mut client = Client {
-            // sender: tx_channel.sender(),
-            sender: tx_pipe_writer,
-            receiver: rx_channel.receiver(),
-        };
+//         let mut client = Client {
+//             // sender: tx_channel.sender(),
+//             sender: tx_pipe_writer,
+//             receiver: rx_channel.receiver(),
+//         };
 
-        info!("About to start tcp futures");
+//         info!("About to start tcp futures");
 
-        match select3(rx_fut, tx_fut, f(&mut client)).await {
-            Either3::First(e) => info!("Finished network comms with read error {:?}", e),
-            Either3::Second(e) => info!("Finished network comms with write error {:?}", e),
-            Either3::Third(_) => info!("Finished network comms by polling completing"),
-        }
+//         match select3(rx_fut, tx_fut, f(&mut client)).await {
+//             Either3::First(e) => info!("Finished network comms with read error {:?}", e),
+//             Either3::Second(e) => info!("Finished network comms with write error {:?}", e),
+//             Either3::Third(_) => info!("Finished network comms by polling completing"),
+//         }
 
-        info!("Finished network comms, will reconnect");
-    }
-}
+//         info!("Finished network comms, will reconnect");
+//     }
+// }
