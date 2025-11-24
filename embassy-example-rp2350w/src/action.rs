490c64c3bbc9b2391450512daeb1a02854879e07
jorge-imperial <jorgeluis.imperial@gmail.com>
2025-11-24 13:16:49 -0800

    Adding Rp235x example.

---
+++ embassy-example-rp2350w/src/action.rs
@@ -0,0 +1,40 @@
+use mountain_mqtt::{
+    client::{Client, ClientError},
+    data::quality_of_service::QualityOfService,
+    mqtt_manager::{ConnectionId, MqttOperations},
+};
+
+pub const TOPIC_BUTTON: &str = "embassy-example-rp2350w-button";
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, defmt::Format)]
+pub enum Action {
+    Button(bool),
+}
+
+impl MqttOperations for Action {
+    async fn perform<'a, 'b, C>(
+        &'b mut self,
+        client: &mut C,
+        _client_id: &'a str,
+        _connection_id: ConnectionId,
+        _is_retry: bool,
+    ) -> Result<(), ClientError>
+    where
+        C: Client<'a>,
+    {
+        match self {
+            Action::Button(pressed) => {
+                let payload = if *pressed { "true" } else { "false" };
+                client
+                    .publish(
+                        TOPIC_BUTTON,
+                        payload.as_bytes(),
+                        QualityOfService::Qos1,
+                        false,
+                    )
+                    .await?;
+            }
+        }
+        Ok(())
+    }
+}
