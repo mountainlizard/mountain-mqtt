03bff142c27e1ada08b2cc10f8c118da56cab118
Marc-Antoine Desrochers <Marc-Antoine@Desrochers.space>
2025-12-05 13:39:24 -0500

    Add authentication methods to ConnectionSettings

--- mountain-mqtt/src/client.rs
+++ mountain-mqtt/src/client.rs
@@ -328,8 +328,42 @@ impl<'a> ConnectionSettings<'a> {
             client_id,
         }
     }
+
+    pub fn with_auth(
+        client_id: &'a str,
+        username: Option<&'a str>,
+        password: Option<&'a [u8]>,
+    ) -> ConnectionSettings<'a> {
+        Self {
+            keep_alive: KEEP_ALIVE_DEFAULT,
+            username,
+            password,
+            client_id,
+        }
+    }
+
+    pub fn authenticated(
+        client_id: &'a str,
+        username: &'a str,
+        password: &'a [u8],
+    ) -> ConnectionSettings<'a> {
+        Self::with_auth(client_id, Some(username), Some(password))
+    }
+
     pub fn client_id(&self) -> &'a str {
         self.client_id
+    }
+
+    pub fn username(&self) -> Option<&'a str> {
+        self.username
+    }
+
+    pub fn password(&self) -> Option<&'a [u8]> {
+        self.password
+    }
+
+    pub fn keep_alive(&self) -> u16 {
+        self.keep_alive
     }
 }
 
